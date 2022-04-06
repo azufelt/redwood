f1e9389880fc069daeb6a7b1d9308fec13678440
Ashley Zufelt <ashley.zufelt@gmail.com>
2022-04-05 21:09:52 -0400

    syntax error )

--- app.js
+++ app.js
@@ -40,91 +40,99 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 const store = new MongoDBStore({
-      uri: `mongodb+srv://herokuAdmin:${PASSWORD}@redwood.akcwx.mongodb.net/redwood-design-shop?retryWrites=true&w=majority`,
-      databaseName: 'redwood',
-      collection: 'sessions'
-    }.catch(err => {
-        console.log('This is getting annoying')
-        console.log(err);
-      );
-      const csrfProtection = csrf();
+  uri: `mongodb+srv://herokuAdmin:${PASSWORD}@redwood.akcwx.mongodb.net/redwood-design-shop?retryWrites=true&w=majority`,
+  databaseName: 'redwood',
+  collection: 'sessions'
+}.catch(err => {
+  console.log('This is getting annoying')
+  console.log(err);
+}));
+const csrfProtection = csrf();
 
 
-      // Make requests
-      app.set('view engine', 'ejs'); app.set('views', 'views');
+// Make requests
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-      const corsOptions = {
-        origin: 'https://redwood-design.herokuapp.com/',
-        optionsSuccessStatus: 200
-      }; app.use(cors(corsOptions));
+const corsOptions = {
+  origin: 'https://redwood-design.herokuapp.com/',
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-      const options = {
-        family: 4
-      };
+const options = {
+  family: 4
+};
 
-      //parse json body of the request
-      app.use(bodyParser.urlencoded({
-        extended: false
-      })); app.use(express.static(path.join(__dirname, 'public'))); app.use(
-        session({
-          secret: 'my secret',
-          resave: false,
-          saveUninitialized: false,
-          store: store
-        })
-      ); app.use(csrfProtection); app.use(flash());
+//parse json body of the request
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
-      //authenticate user
-      app.use((req, res, next) => {
-        res.locals.isAuthenticated = req.session.isLoggedIn;
-        res.locals.csrfToken = req.csrfToken();
-        next();
-      });
+//authenticate user
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
-      app.use((req, res, next) => {
-        if (!req.session.user) {
-          return next();
-        }
-        User.findById(req.session.user._id)
-          .then(user => {
-            if (!user) {
-              return next();
-            }
-            req.user = user;
-            next();
-          })
-          .catch(err => {
-            next(new Error(err));
-          });
-      });
-
-
-      // Using our routes as defined.
-      app.use('/admin', adminRoutes); app.use(shopRoutes); app.use(authRoutes);
-
-      app.get('/500', errorController.get500);
-
-      app.use(errorController.get404);
-
-      app.use((error, req, res, next) => {
-        res.status(500).render('500', {
-          pageTitle: 'Error!',
-          path: '../500',
-          // isAuthenticated: req.session.isLoggedIn
-        });
-      });
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
 
 
-      mongoose
-      .connect(MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-      })
-      .then(result => {
-        app.listen(process.env.PORT, '0.0.0.0');
-        console.log("connection successful");
-      })
-      .catch(err => {
-        console.log(err);
-      });
+// Using our routes as defined.
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '../500',
+    // isAuthenticated: req.session.isLoggedIn
+  });
+});
+
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(result => {
+    app.listen(process.env.PORT, '0.0.0.0');
+    console.log("connection successful");
+  })
+  .catch(err => {
+    console.log(err);
+  });
